db0d60cc2e8eae661d68a3f260a1173be63a9c6d
clcamou <clcamou@gmail.com>
2019-12-13 21:17:09 -0700

    fixed spacing issue with city list

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,308 +1,117 @@
 $(document).ready(function () {
-    let test = false;
-    // use school key inorder to get forecast data
-    let APIKey = "ad5b05f2107956afacca6e7b08b1f854";
-    let url = 'https://api.openweathermap.org/data/2.5/';
-    let requestType = ""; 
-    let query ="";
-    //
-  
-    // pull current location
-      $('#getWeather,#past-cities').on('click', function () {
-      if (test) console.log("on click");
-      // get location from user input box
-      let e = $(event.target)[0];
-      let location = "";
-  
-      if (e.id === "getWeather" || e.id === "getWeatherId") {
-        if (test) console.log("getWeather");
-        location = $('#city-search').val().trim().toUpperCase();
-      } else if ( e.className === ("cityList") ) {
-        if (test) console.log("cityList");
-        location = e.innerText;
-      }
-  
-      // should make this generic to use this on the area clicked
-      // let location = $(this).val().trim().toUpperCase();
-      if (test) { console.log('location:' + location); }
-      if (location == "") return;
-  
-      updateCityStore(location);
-      getCurWeather(location);
-      getForecastWeather(location);
-     });
-  
-    function convertDate(epoch) {
-      // function to convert unix epoch to local time
-      // returns arr ["MM/DD/YYYY, HH:MM:SS AM", "MM/DD/YYYY", "HH:MM:SS AM"]
-      if (test) { console.log(`convertData - epoch: ${epoch}`); }
-      let readable = [];
-      let myDate = new Date(epoch * 1000);
-  
-      // local time
-      // returns string "MM/DD/YYYY, HH:MM:SS AM"
-      readable[0] = (myDate.toLocaleString());
-      readable[1] = ((myDate.toLocaleString().split(", "))[0]);
-      readable[2] = ((myDate.toLocaleString().split(", "))[1]);
-  
-      if (test) { console.log(` readable[0]: ${readable[0]}`); }
-      return readable;
+    //click listeners to activate the functions and Calls
+    $("#search-btn").click(weatherNow);
+    $("#search-btn").click(fiveDayForecast);
+    $("#cityList").click(weatherNow);
+    $("#cityList").click(fiveDayForecast);
+
+    let userInput;
+    let time = moment().format("LL");
+    let myKey = "ad5b05f2107956afacca6e7b08b1f854";
+    $(".figure").css("display", "none");
+    // $("#search-container").css("left", "320px");
+    $(".ul-container").css("display", "none");
+
+    //Function to call current weather  
+    function weatherNow(event) {
+        event.preventDefault();
+        //will target the value of the user userInput or the saved list
+        if ($(this).attr("id") === "cityList") {
+            let x = event.target;
+            userInput = $(x).text();
+            console.log(userInput);
+        } else {
+            userInput = $(this).prev().val(); //getting value of user input
+        }
+        $(".figure").empty(); //empty search results upon each new search
+        $("#search-container").animate({ "left": "10px"}, 600);
+        $(".ul-container").css("display", "flex");
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&APPID=" + myKey;
+        //calling the API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $(".figure").css("display", "block");
+            //setting the values to a new DOM element 
+            let city = $("<h1>").addClass("city-name").text(`City: ${response.name}`);
+            let date = $("<h3>").addClass("date").text(`Date: ${time}`);
+            let iconImage = $("<img>").addClass("icon-image").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+            let tempF = parseInt((response.main.temp - 273.15) * 1.8 + 32); //kelvin to farenheight Conversion
+            let temperature = $("<h4>").addClass("current-temp").text(`Current Temperature: ${tempF} F°`);
+            let humidity = $("<h4>").addClass("humidity").text(`Humidity: ${response.main.humidity}%`);
+            let windSpeed = $("<h4>").addClass("wind-speed").text(`Wind Speed ${response.wind.speed} mph`);
+            //Appending the values to the figure box
+            $(".figure").append(city, iconImage, date, temperature, humidity, windSpeed);
+
+        })
+
     }
-  
-    function getCurLocation() {
-      // This function is based on geoFindMe function found at
-      //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
-      //this function return an object with the lat and lon of current location
-      if (test) { console.log("getCurLocation"); }
-  
-      let location = {};
-  
-      function success(position) {
-        if (test) { console.log(" success"); }
-        if (test) { console.log("  location", position); }
-  
-        location = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          success: true
+    //end current day call begin Five day forecast call
+    function fiveDayForecast() {
+        if ($(this).attr("id") === "cityList") {
+            let x = event.target;
+            userInput = $(x).text();
+            console.log(userInput);
+        } else {
+            userInput = $(this).prev().val(); //getting value of user input
         }
-        if (test) { console.log(" success location", location); }
-        getCurWeather(location);
-        getForecastWeather(location);
-      }
-  
-      function error() {
-        location = { success: false }
-        console.log('Could not get location');
-        return location;
-      }
-  
-      if (!navigator.geolocation) {
-        console.log('Geolocation is not supported by your browser');
-      } else {
-        navigator.geolocation.getCurrentPosition(success, error);
-      }
-    };
-  
-    function getCurWeather(loc) {
-      // function to get current weather
-      // returns object of current weather data
-      if (test) { console.log("getCurWeather - loc:", loc); }
-      if (test) { console.log("getCurWeather - toloc:",typeof loc); }
-  
-      drawHistory();
-      // clear search field
-      $('#city-search').val("");
-  
-      if (typeof loc === "object") {
-        city = `lat=${loc.latitude}&lon=${loc.longitude}`;
-      } else {
-        city = `q=${loc}`;
-      }
-  
-      // set queryURL based on type of query
-      requestType = 'weather';
-      query = `?${city}&units=imperial&appid=${APIKey}`;
-      queryURL = `${url}${requestType}${query}`;
-  
-      if (test) console.log(`cur queryURL: ${queryURL}`);
-      // Create an AJAX call to retrieve data Log the data in console
-      $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(function (response) {
-        if (test) console.log(response);
-  
-        weatherObj = {
-          city: `${response.name}`,
-          wind: response.wind.speed,
-          humidity: response.main.humidity,
-          temp: response.main.temp,
-          date: (convertDate(response.dt))[1],
-          icon: `http://openweathermap.org/img/w/${response.weather[0].icon}.png`,
-          desc: response.weather[0].description
-        }
-  
-        // calls function to draw results to page
-        drawCurWeather(weatherObj);
-        getUvIndex(response);
-      });
-    };
-  
-    function getForecastWeather(loc) {
-      // function to get 5 day forecast data
-      // returns array of daily weather objects
-      if (test) { console.log("getForecastWeather - loc:", loc); }
-  
-  
-  
-      if (typeof loc === "object") {
-        city = `lat=${loc.latitude}&lon=${loc.longitude}`;
-      } else {
-        city = `q=${loc}`;
-      }
-  
-      // array to hold all the days of results
-      let weatherArr = [];
-      let weatherObj = {};
-  
-      // set queryURL based on type of query
-      requestType = 'forecast/daily';
-      query = `?${city}&cnt=6&units=imperial&appid=${APIKey}`;
-      queryURL = `${url}${requestType}${query}`;
-  
-      // Create an AJAX call to retrieve data Log the data in console
-      $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(function (response) {
-        if (test) console.log("getForecast response",response);
-  
-        for (let i = 1; i < response.list.length; i++) {
-          let cur = response.list[i]
-          // TODO check for errors/no data
-          weatherObj = {
-            weather: cur.weather[0].description,
-            icon: `http://openweathermap.org/img/w/${cur.weather[0].icon}.png`,
-            minTemp: cur.temp.min,
-            maxTemp: cur.temp.max,
-            humidity: cur.humidity,
-            date: (convertDate(cur.dt))[1]
-          };
-  
-          weatherArr.push(weatherObj);
-        }
-  
-        drawForecast(weatherArr);
-      });
-    };
-  
-    function drawCurWeather(cur) {
-      // function to draw  weather all days
-      // need logic to pick variables
-      if (test) { console.log('drawCurWeather - cur:', cur); }
-  
-      $('#forecast').empty(); 
-      $('#cityName').text(cur.city + " (" + cur.date + ")");
-      $('#curWeathIcn').attr("src", cur.icon);
-      $('#curTemp').text("Temp: " + cur.temp + " F");
-      $('#curHum').text("Humidity: " + cur.humidity + "%");
-      $('#curWind').text("Windspeed: " + cur.wind + " MPH");
-    };
-  
-    function drawForecast(cur) {
-      if (test) { console.log('drawForecast - cur:', cur); }
-  
-      for (let i = 0; i < cur.length; i++) {
-        let $colmx1 = $('<div class="col mx-1">');
-        let $cardBody = $('<div class="card-body forecast-card">');
-        let $cardTitle = $('<h5 class="card-title">');
-        $cardTitle.text(cur[i].date);
-  
-  
-        let $ul = $('<ul>');
-  
-        let $iconLi = $('<li>');
-        let $iconI = $('<img>');
-        $iconI.attr('src', cur[i].icon);
-  
-        let $weathLi = $('<li>');
-        $weathLi.text(cur[i].weather);
-  
-        let $tempMinLi = $('<li>');
-        $tempMinLi.text('Min Temp: ' + cur[i].minTemp + " F");
-  
-        let $tempMaxLi = $('<li>');
-        $tempMaxLi.text('Max Temp: ' + cur[i].maxTemp + " F");
-  
-        let $humLi = $('<li>');
-        $humLi.text('Humidity: ' + cur[i].humidity + "%");
-  
-        // assemble element
-        $iconLi.append($iconI);
-  
-        $ul.append($iconLi);
-        $ul.append($weathLi);
-        $ul.append($tempMinLi);
-        $ul.append($tempMaxLi);
-        $ul.append($humLi);
-  
-        $cardTitle.append($ul);
-        $cardBody.append($cardTitle);
-        $colmx1.append($cardBody);
-  
-        $('#forecast').append($colmx1);
-      }
-    };
-  
-    function getUvIndex(uvLoc) {
-      if (test) { console.log('getUvIndex loc:',uvLoc); }
-      // function to color uv index
-  
-        city = `lat=${parseInt(uvLoc.coord.lat)}&lon=${parseInt(uvLoc.coord.lon)}`;
-  
-      // set queryURL based on type of query
-      requestType = 'uvi';
-      query = `?${city}&appid=${APIKey}`;
-      queryURL = `${url}${requestType}${query}`;
-  
-      // Create an AJAX call to retrieve data Log the data in console
-      $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(function (response) {
-        let bkcolor = "violet";
-        
-        // if (test) response.value = 7.1234567;
-  
-        let uv = parseFloat(response.value);
-  
-        if (uv < 3) { 
-          bkcolor = 'green';
-        } else if (uv < 6) { 
-          bkcolor = 'yellow';
-        } else if (uv < 8) { 
-          bkcolor = 'orange';
-        } else if (uv < 11) { 
-          bkcolor = 'red';
-        }
-  
-        let title = '<span>UV Index: </span>';
-        let color = title + `<span style="background-color: ${bkcolor}; padding: 0 7px 0 7px;">${response.value}</span>`;
-  
-        $('#curUv').html(color);
-      }); 
-    };
-  
-    function updateCityStore(city) {
-      let cityList = JSON.parse(localStorage.getItem("cityList")) || [];
-      cityList.push(city); 
-      cityList.sort();
-  
-      // removes dulicate cities
-      for (let i=1; i<cityList.length; i++) {
-         if (cityList[i] === cityList[i-1]) cityList.splice(i,1);
-      }
-  
-      //stores in local storage
-      localStorage.setItem('cityList', JSON.stringify(cityList));
-    };
-  
-    function drawHistory() {
-      // function to pull city history from local memory
-      if (test) console.log('getHistory');
-      let cityList = JSON.parse(localStorage.getItem("cityList")) || [];
-  
-      $('#past-cities').empty();
-      cityList.forEach ( function (city) {
-        let cityNameDiv = $('<div>');
-        cityNameDiv.addClass("cityList");
-        cityNameDiv.attr("value",city);
-        cityNameDiv.text(city);
-        $('#past-cities').append(cityNameDiv);
-      });
-    };
-  
-    // will get location when page initializes
-    const location = getCurLocation();
-  });
+        let dayDisplay = 1;
+        let fiveDayCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&APPID=" + myKey;
+        //calling the 5 day forecast
+        $.ajax({
+            url: fiveDayCall,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            let listArray = response.list;
+            listArray.forEach(element => {   //use for each method to loop through object list
+                //   console.log(element);
+                let yearDateTime = element.dt_txt;
+                //    console.log (yearDatetime);    
+                let currentDate = yearDateTime.split(" ")[0]; //splitting the full date
+                let currentTime = yearDateTime.split(" ")[1]; //and time  in the object
+                
+                //getting a specific time of day to pull data from and insert inot the DOM
+                if (currentTime === "15:00:00") {
+                    let day = currentDate.split("-")[2];
+                    let month = currentDate.split("-")[1];
+                    let year = currentDate.split("-")[0];
+                    $("#day-" + dayDisplay).children(".date-display").html(`${month}/${day}/${year}`);
+                    $("#day-" + dayDisplay).children("#daily-icon").attr("src", "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png");
+                    $("#day-" + dayDisplay).children("#daily-temp").html(`Temperature: ${parseInt((element.main.temp - 273.15) * 1.8 + 32)}°F`);
+                    $("#day-" + dayDisplay).children("#5day-humidity").html(`Humidity: ${element.main.humidity}% `);
+                    dayDisplay++
+
+                }
+            })
+        })
+    }
+
+    //-------------------------Local Storage-----------//
+    let ul = $("#cityList");
+    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+    let data = JSON.parse(localStorage.getItem('items'));
+
+    let liMaker = text => {
+        let li = $('<li>').addClass("created-city btn btn-light");
+        li.text(text);
+        ul.prepend(li);
+    }
+    $("#search-btn").click(function () {
+        itemsArray.push(userInput);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(userInput);
+    })
+
+    data.forEach(item => {
+        liMaker(item);
+        console.log(item);
+    })
+    $(".clr-btn").on("click", function () {
+        $(".created-city").remove();
+        localStorage.clear();
+        $("input").empty();
+    })
+})
